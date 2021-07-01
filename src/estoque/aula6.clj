97264b8041601550f052d0aca3ddb2707ef225ed
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-07-01 19:53:05 -0300

    feat: Filtering maps and creating comps with comp

--- src/estoque/aula6.clj
+++ src/estoque/aula6.clj
@@ -51,3 +51,35 @@
        (reduce +)))
 
 (println "Total do pedido:" (total-do-pedido pedido))
+
+(def pedido {:mochila {:quantidade 2, :preco 80}
+             :camiseta {:quantidade 3, :preco 40}
+             :chaveiro {:quantidade 1}})
+
+(defn gratuito?
+  [[_ item]]
+  (<= (get item :preco 0) 0))
+
+(println "Filtrando gratuitos")
+(println (filter gratuito? pedido ))
+
+
+(defn gratuito?
+  [item]
+  (<= (get item :preco 0) 0))
+
+(println "Filtrando gratuitos")
+(println (filter (fn [[_ item]] (gratuito? item)) pedido))
+
+(println (filter #(gratuito? (second %)) pedido))
+
+(defn pago?
+  [item]
+  (not (gratuito? item)))
+
+(println (pago? {:preco 50}))
+(println (pago? {:preco 0}))
+
+(def pago? (comp not gratuito?))
+(println (pago? {:preco 50}))
+(println (pago? {:preco 0}))
